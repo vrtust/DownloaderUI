494341082832d5560eba1402ba14cf261491f3a6
vrtust <sosvrt@outlook.com>
2024-09-03 11:00:11 +0800

    update extension and change method to get file size when parse

--- DownloaderUI/ViewModels/DownloadPageViewModel.cs
+++ DownloaderUI/ViewModels/DownloadPageViewModel.cs
@@ -206,7 +206,8 @@ namespace DownloaderUI.ViewModels
                 httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
                 try
                 {
-                    HttpResponseMessage response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, Url));
+                    // HttpResponseMessage response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, Url));
+                    HttpResponseMessage response = await httpClient.GetAsync(Url, HttpCompletionOption.ResponseHeadersRead);
 
                     if (response.IsSuccessStatusCode)
                     {
